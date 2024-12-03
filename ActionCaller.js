7dd1d0ea2ed4934539e0de05e1eae3184facf62d
olaniyimalik101 <olaniyimalik908@gmail.com>
2024-12-03 02:41:11 -0600

    Update ActionCaller.js

--- ActionCaller.js
+++ ActionCaller.js
@@ -1,98 +1,106 @@
-function fetchMotorClaimDetail(executionContext) {
-  let formContext = executionContext.getFormContext();
-  let nationalId = formContext.getAttribute("vrp_nationalid").getValue();
-  let claimnumber = formContext.getAttribute("vrp_name").getValue();
-  let lineOfBiz = formContext.getAttribute("ey_lineofbusiness").getValue();
-  let claimDetailFetched = false;
+function fetchMotorClaimDetail(primaryControl) {
+    let formContext = primaryControl;
+    let duplicateOption = formContext.getAttribute("cmpp_duplicatehandling").getValue();
+    let pluginCalled = false;
 
+    // Check if duplicateOption is null or undefined
+    if (duplicateOption == null) {
+        var mesgTitle = "Duplicate Handling Is Empty";
+        var details = "Duplicate Handling is compulsory. Please select an option.";
+        Xrm.Navigation.openErrorDialog({ message: mesgTitle, details: details });
+        return;
+    }
 
+    if (!pluginCalled) {
+        var objActionCallRequest = null;
+        let actionName = "cmpp_validateandimportfile";
 
-  if (!claimDetailFetched) {
-    if (nationalId == null || claimnumber == null) {
-      return;
-    }
+        var parameters = {};
+        var entity = {};
+        entity.id = formContext.data.entity.getId().replace("{", "").replace("}", "");
+        entity.entityType = "cmpp_importfile";
+        parameters.entity = entity;
+        parameters.DuplicateHandlingOption = duplicateOption;
 
+        try {
+            objActionCallRequest = {
+                entity: parameters.entity,
+                DuplicateHandlingOption: parameters.DuplicateHandlingOption,
+                getMetadata: function () {
+                    return {
+                        boundParameter: "entity",
+                        parameterTypes: {
+                            "entity": {
+                                "typeName": "mscrm.cmpp_importfile",
+                                "structuralProperty": 5
+                            },
+                            "DuplicateHandlingOption": {
+                                "typeName": "Edm.String",
+                                "structuralProperty": 1
+                            }
+                        },
+                        operationType: 0,
+                        operationName: actionName
+                    };
+                }
+            };
 
+            Xrm.Utility.showProgressIndicator("Processing File Validation and Import...");
 
-    if (lineOfBiz != null && lineOfBiz == 364840001) {
-      var objActionCallRequest = null;
-      let actionName = "vrp_GetMotorClaimsDetails";
+            Xrm.WebApi.online.execute(objActionCallRequest).then(
+                function success(result) {
+                    if (result.status === 200) {  // Checking for success status
+                        result.json().then(function (response) {
+                            if (response) {
+                                console.log("Respons: " + response.Response);
+                                showValidationandImportUpdate(response.Response);
+                            }
+                        });
+                    } else {
+                        Xrm.Navigation.openErrorDialog({ message: "Issue in File Import", details: "Import execution failed" });
+                    }
+                    Xrm.Utility.closeProgressIndicator();
+                },
+                function (ex) {
+                    Xrm.Navigation.openErrorDialog({ message: "Error: " + ex.message, details: ex.message });
+                    Xrm.Utility.closeProgressIndicator();
+                }
+            );
+        } catch (ex) {
+            Xrm.Navigation.openErrorDialog({ message: ex.message, details: ex.message });
+            Xrm.Utility.closeProgressIndicator();
+        }
+    }
+}
 
+function showValidationandImportUpdate(validationResponse) {
+    // Close the progress indicator before displaying the result
+    Xrm.Utility.closeProgressIndicator();
 
+    // If validation is not successful
+    if (!validationResponse.IsSuccess) {
+        var errorMessages = validationResponse.failureReason.join("\n");
 
-      var parameters = {};
-      var entity = {};
-      entity.id = formContext.data.entity.getId().replace("{", "").replace("}", "");
-      entity.entityType = "vrp_claim";
-      parameters.entity = entity;
-      parameters.NationalID = nationalId;
-      parameters.ClaimNumber = claimnumber;
+        var message = "Validation Failed:\n" + errorMessages;
+        var alertStrings = {
+            confirmButtonLabel: "OK",
+            text: message,
+            title: "Validation Errors"
+        };
 
+        var alertOptions = { height: 120, width: 450 };
+        Xrm.Navigation.openAlertDialog(alertStrings, alertOptions);
+    }
+    // If validation is successful
+    else {
+        var successMessage = "File validation is successful. Import will run in the background. Please check the Import Record tab for import details.";
+        var successAlertStrings = {
+            confirmButtonLabel: "OK",
+            text: successMessage,
+            title: "Validation Successful"
+        };
 
-
-      try {
-        objActionCallRequest = {
-          entity: parameters.entity,
-          NationalID: parameters.NationalID,
-          ClaimNumber: parameters.ClaimNumber,
-
-
-
-          getMetadata: function () {
-            return {
-              boundParameter: "entity",
-              parameterTypes: {
-                "entity": {
-                  "typeName": "mscrm.vrp_claim",
-                  "structuralProperty": 5
-                },
-                "NationalID": {
-                  "typeName": "Edm.String",
-                  "structuralProperty": 1
-                },
-                "ClaimNumber": {
-                  "typeName": "Edm.String",
-                  "structuralProperty": 1
-                }
-              },
-              operationType: 0,
-              operationName: actionName
-            };
-          }
-        };
-
-
-
-        Xrm.Utility.showProgressIndicator("Fetching Motor Claim Detail...");
-        Xrm.WebApi.online.execute(objActionCallRequest).then(
-          function success(result) {
-            console.log(result);
-            if (result.ok) {
-              result.json().then(function (response) {
-                if (response != null) {
-                  Xrm.Utility.closeProgressIndicator();
-                  if (response.IsSuccess) {
-                    claimDetailFetched = true;
-                    console.log('API successfully retrieved the claim detail');
-                    formContext.data.save(); // Save the form context
-                    Xrm.Utility.closeProgressIndicator();
-                  }
-                }
-              });
-            } else {
-              console.log("Issue from Plugin");
-            }
-          },
-          function (ex) {
-            Xrm.Navigation.openErrorDialog({ message: "Error :: " + ex.message, details: ex.message });
-            Xrm.Utility.closeProgressIndicator();
-          }
-        );
-      }
-      catch (ex) {
-        Xrm.Navigation.openErrorDialog({ message: ex.message, details: ex.message });
-        Xrm.Utility.closeProgressIndicator();
-      }
-    }
-  }
+        var successAlertOptions = { height: 120, width: 450 };
+        Xrm.Navigation.openAlertDialog(successAlertStrings, successAlertOptions);
+    }
 }
